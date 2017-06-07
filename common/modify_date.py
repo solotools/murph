4c83d5c76e0c90dbc41f8d67b46e26b5e0218257
Pactortester <1456470136@qq.com>
2017-06-07 12:30:14 +0800

    陈艳 add address

--- common/modify_date.py
+++ common/modify_date.py
@@ -1,10 +1,8 @@
 import os
 
-from green import modify
 
 
 def set_sys_time(year, month, day):
     os.system('date %04d/%02d/%02d' % (year, month, day))
 
-set_sys_time(2019, 6, 13)
-modify()
+set_sys_time(2020, 6, 30)
